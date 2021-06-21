79b73faa9528d20f3268760934718d3e0f4aaf9d
Lamia Haider <lamia.haider@gmail.com>
2021-06-21 19:52:27 -0400

    Spec not workinng

--- recipe.js
+++ recipe.js
@@ -1,1 +1,25 @@
 'use strict';
+
+// var chocolateRecipe = {chocolate: "16 oz"}
+var recipe = { ingredient: 'amount' }
+
+function addIngredient(recipe, ingredient, amount){
+  recipe[ingredient] = amount;
+  return recipe;
+}
+
+function removeIngredient(recipe, ingredient){
+  delete recipe[ingredient];
+  return recipe;
+}
+
+function updateIngredient(recipe, ingredient, amount){
+  recipe[ingredient] = amount;
+  return recipe;
+}
+
+function readRecipe(recipe){
+  for (var key in recipe) {
+    console.log("this recipe calls for " + recipe[key] + " of " + key);
+  }
+}
